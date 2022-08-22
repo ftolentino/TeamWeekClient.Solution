f9ea7f65ced61d1c3d59cb2ca1d6287978c2b754
Alex Shevlin <alexshevlin1@gmail.com>
2022-08-22 14:11:36 -0700

    update connection link.

--- TeamWeekClient/Program.cs
+++ TeamWeekClient/Program.cs
@@ -21,6 +21,7 @@ namespace TeamWeekClient
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:5004/");
                 });
     }
 }
