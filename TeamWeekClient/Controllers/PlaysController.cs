b695acbf528a9f8f8c20d74ebf08ea17ac899349
ryandefea <ryan.defea@gmail.com>
2022-08-22 18:29:02 -0500

    create plays controller
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>

---
+++ TeamWeekClient/Controllers/PlaysController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TeamWeekClient.Models;
+
+namespace TeamWeekClient.Controllers
+{
+  public class PlaysController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
